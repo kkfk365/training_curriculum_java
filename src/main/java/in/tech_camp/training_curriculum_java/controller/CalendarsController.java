ce133124b3c3ffa29b6d1e3f2c3d2571afc378b3
kkfk365 <fumiaki.koike@smart-group.co.jp>
2026-05-22 10:45:08 +0900

    Isuue2

--- src/main/java/in/tech_camp/training_curriculum_java/controller/CalendarsController.java
+++ src/main/java/in/tech_camp/training_curriculum_java/controller/CalendarsController.java
@@ -56,7 +56,7 @@ public class CalendarsController {
     String[] wdays = {"(日)", "(月)", "(火)", "(水)", "(木)", "(金)", "(土)"};
 
     for (int x = 0; x < 7; x++) {
-      Map<String, Object> newMap = new HashMap<>();
+      Map<String, Object> newMap = new HashMap<String, Object>();
       LocalDate currentDate = todaysDate.plusDays(x);
 
       List<String> todayPlans = new ArrayList<>();
